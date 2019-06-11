0beaa01a78f394bc7a3ae32595df8f30bb44a6fe
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-06-11 19:42:46 -0300

    TodoList component

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <div>
+        todo list
+      </div>
+    );
+  }
+}
+
+export default TodoList;
