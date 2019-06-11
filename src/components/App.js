55f7bedc483e9d39b202bb93386d8d2df8cbc994
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-06-11 19:49:05 -0300

    Importing TodoList and App.scss inside App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './App.scss';
+import TodoList from './TodoList/TodoList';
 
 function App() {
   return (
     <div className="App">
-      Todo List
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <main>
+        <TodoList />
+      </main>
     </div>
   );
 }
