5a1fadf59a30ddcd6a91a1caef3b2f24cf4f8118
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-06-12 02:02:44 -0300

    Adding noopener noreferrer flags into About links to remove warnings

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,25 +8,25 @@ function About () {
         <p>Stack used to make this app:</p>
         <ul>
           <li>
-            <a href="https://reactjs.org/" target="_blank">
+            <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
               React
             </a>
           </li>
           <li>
-            <a href="https://sass-lang.com/" target="_blank">
+            <a href="https://sass-lang.com/" target="_blank" rel="noopener noreferrer">
               Sass
             </a>
           </li>
           <li>
-            <a href="https://jsonplaceholder.typicode.com/" target="_blank">
+            <a href="https://jsonplaceholder.typicode.com/" target="_blank" rel="noopener noreferrer">
               JSONPlaceholder
             </a>
           </li>
         </ul>
         <p>
-          Many thanks to <a href="https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA" target="_blank">
+          Many thanks to <a href="https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA" target="_blank" rel="noopener noreferrer">
             Traversy Media
-          </a> for this <a href="https://www.youtube.com/watch?v=sBws8MSXN7A" target="_blank">
+          </a> for this <a href="https://www.youtube.com/watch?v=sBws8MSXN7A" target="_blank" rel="noopener noreferrer">
             tutorial
           </a> on React.
         </p>
