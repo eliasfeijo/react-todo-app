0ee9a4786b60107db4dd1d0a6f0624f3c29fad9c
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-06-12 01:57:55 -0300

    Adding Links to Home and About routes

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     return (
       <header>
         <h1>Todo List</h1>
+        <p>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </p>
       </header>
     );
   }
