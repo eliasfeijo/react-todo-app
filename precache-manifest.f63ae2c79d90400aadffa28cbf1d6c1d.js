db0215095b6db858048aa5085c1a103331ff7583
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-06-12 02:23:47 -0300

    Updates

---
+++ precache-manifest.f63ae2c79d90400aadffa28cbf1d6c1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7d7133ec638f91ba52071eb208c4b0",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "da3fa96df7674e97eed8",
+    "url": "/react-todo-app/static/css/main.f9a63bf6.chunk.css"
+  },
+  {
+    "revision": "e70552bfddb2456c3315",
+    "url": "/react-todo-app/static/js/2.266f34b0.chunk.js"
+  },
+  {
+    "revision": "da3fa96df7674e97eed8",
+    "url": "/react-todo-app/static/js/main.ae84d3af.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
